d7f8180976383558750148aaa89cc8bc9df776b5
Everton Araujo <40547014+evertonsaraujo@users.noreply.github.com>
2018-08-15 21:46:24 -0300

    Add files via upload

--- pipe_as_pandas.py
+++ pipe_as_pandas.py
@@ -1,14 +1,23 @@
 import pandas
-import numpy
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 import pandas as pd
-import datetime
+#import datetime
 import os
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import xlsxwriter
 
 #VARIAVEIS GLOBAIS
 
 # REALIZAR BACKUP DAS PLANILHAS
 
+# Salvando backup pipeline
+#pipearq = '"Pipeline HANDEL.xlsx"'
+#pipebkp = '"Pipeline HANDEL-bkp.xlsx"'
+#pipefile = os.system('cp ' + pipearq + ' ' + pipebkp)
+#os.system('mkdir backup')
+#os.system('mv ' + pipebkp + ' backup')
 
 # ABRE PLANILHA GSA
 gsa = pd.ExcelFile('Status GSA Orders 23.05.xlsx')
@@ -23,17 +32,29 @@ gsadf.to_excel('Status GSA Orders 23.05.xlsx', sheet_name='2018')
 
 # ABRE PLANILHA PIPELINE
 pipeline = pd.ExcelFile('Pipeline HANDEL.xlsx')
-pipedf = pd.read_excel(pipeline, 'PIPELINE')
-pipedf.to_excel('Pipeline HANDEL.xlsx', sheet_name='PIPELINE')
-for index, row in pipedf.iterrows():
+pipedf = {sheet_name: pipeline.parse(sheet_name)
+          for sheet_name in pipeline.sheet_names}
+pipepipe = pd.read_excel(pipeline,sheet_name='PIPELINE')
+
+# ESCREVE NOVA PLANILHA PIPELINE
+dfpipenew = pd.DataFrame.copy(pipepipe)
+pipenew = ExcelWriter('Pipelinenew.xlsx', engine='xlsxwriter')
+dfpipenew.to_excel(pipenew, sheet_name='PIPELINE')
+#pipenew.save()
+#print(dfpipenew.head())
+
+
+for index, row in pipepipe.iterrows():
     for index, rowgsa in gsadf.iterrows():
         if row['IMP'] == rowgsa['IMP NR.']:
             if pandas.isna(rowgsa['CURRENT\nETD']):
                 print('Pipeline IMP: ', row['IMP'], ' - ', 'GSA IMP: ', rowgsa['IMP NR.'], ' -- DATA NAO INFORMADA\n')
             elif rowgsa['CURRENT\nETD'] > row['ETD_PREVISTO']:
-                    print('IMP Pipeline', row['IMP'], row['ETD_PREVISTO'], '--', 'IMP GSA', rowgsa['IMP NR.'], rowgsa['CURRENT\nETD'], ' -- Data Desatualizada\n')
-                    #pipedf.apply(func=setattr()) ESTUDANDO AQUI...
+                    print('Pipeline IMP: ', row['IMP'], row['ETD_PREVISTO'], '--', 'IMP GSA', rowgsa['IMP NR.'], rowgsa['CURRENT\nETD'], ' -- Data Desatualizada\n')
+                    # Atualizar data em Planilha Pipeline
             elif rowgsa['CURRENT\nETD'] < row['ETD_PREVISTO']:
                     print('Pipeline IMP: ', row['IMP'], ' - ', 'GSA IMP: ', rowgsa['IMP NR.'], rowgsa['CURRENT\nETD'], ' -- ATENCAO - DATA MAIOR EM PIPELINE\n')
             elif rowgsa['CURRENT\nETD'] == row['ETD_PREVISTO']:
                     print('Pipeline IMP: ', row['IMP'], row['ETD_PREVISTO'], ' - ', 'GSA IMP: ', rowgsa['IMP NR.'], rowgsa['CURRENT\nETD'], ' -- DATA PIPELINE ATUALIZADA.. TUDO OK!\n')
+pipenew.save()
+pipenew.close()
